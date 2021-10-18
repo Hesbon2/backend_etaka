99dc184e04fbe5c1ebf633b87b030285438fe063
Sohanuzzaman Soad <mdsoad@gmail.com>
2021-10-18 16:16:02 +0600

    Balance Update

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,11 +1,23 @@
+from phone_verify.models import SMSVerification
 from rest_framework import serializers
 from .models import Customer, ClientUser, Merchant, CashOutAgent
 
 
 class ClientUserSerializer(serializers.ModelSerializer):
+    balance = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = ClientUser
         fields = '__all__'
+
+    def get_balance(self, obj):
+        request = self.context
+        token = request.headers.get('Authorization')
+        token_obj = SMSVerification.objects.get(session_token=token)
+        mobile = token_obj.phone_number
+        client = ClientUser.objects.get(mobile=mobile)
+        customer = Customer.objects.get(user=client)
+        return customer.balance
 
 
 class CustomerSerializer(serializers.ModelSerializer):
--- accounts/views.py
+++ accounts/views.py
@@ -15,7 +15,7 @@ class CustomerList(generics.ListCreateAPIView):
 #     queryset = Snippet.objects.all()
 #     serializer_class = SnippetSerializer
 
-class DetailsByToken(generics.RetrieveAPIView):
+class DetailsByToken(APIView):
     def get(self, request):
         token = self.request.headers.get('Authorization')
         print("TOKEN::", token)
@@ -24,7 +24,8 @@ class DetailsByToken(generics.RetrieveAPIView):
             mobile = token_obj.phone_number
             print(mobile)
             client = ClientUser.objects.get(mobile=mobile)
-            serializer = ClientUserSerializer(instance=client)
+            print(client)
+            serializer = ClientUserSerializer(instance=client, context=request)
             return Response(serializer.data)
         except:
             return Response({'error': 'not found'},status= status.HTTP_404_NOT_FOUND )
